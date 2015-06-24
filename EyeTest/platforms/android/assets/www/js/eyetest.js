8181142f21b714c03adfcfc08832a18b5eb2abc1
khanh <npkhanh2311@gmail.com>
2015-06-24 13:04:56 +0700

    - update layout

--- EyeTest/platforms/android/assets/www/js/eyetest.js
+++ EyeTest/platforms/android/assets/www/js/eyetest.js
@@ -21,8 +21,16 @@ var randomHexColor = function() {
 };
 var ui = {
 	gameover: '<div class="game-over text-center col-sm-12 col-md-12 col-lg-12">'+
-                '<div class="start-game col-sm-12 col-md-12 col-lg-12">'+
+                '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">'+
+                    '<button class="btn btn-default share-game"><i class="fa fa-share-alt-square fa-4x text-info"></i></button>'+
                     '<button class="btn btn-default start-game"><i class="fa fa-play fa-4x text-success"></i></button>'+
+                '</div>'+
+                '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">'+
+                '<button class="btn btn-default achievement-game"><i class="fa fa-trophy fa-2x text-info"></i></button>'+
+                '<button class="btn btn-default leaderboard-game"><i class="fa fa-area-chart fa-2x text-info"></i></button>'+
+                '</div>'+
+                '<div class="row">'+
+                '<button class="btn btn-default rate-game"><i class="fa fa-star fa-2x text-info"></i></a></button>'+
                 '</div>'+
             '</div>'
 };
@@ -41,54 +49,59 @@ EyeTest.prototype.init = function(container) {
 	self = this;
 	this.container = container;
 	this.start();
+	console.log(self.container.width());
 };
 EyeTest.prototype.start = function() {
 	
 	var box = $('.eyetest-contain', self.container);
+	var time = $('.time-count', self.container);
+	var error = $('.error-count', self.container);
 	box.html("");
+	error.text(self.errors);
+	time.text(self.timing);
 	var color = randomHexColor();
-	var width = self.container.width() > WIDTH ? self.container.width() / 2 - 50 : self.container.width() - 50;
-	var specialCell = {
-		col: Math.floor((Math.random() * self.size)),
-		row: Math.floor((Math.random() * self.size))
-	};
-	for (var i = 0; i < self.size; i++) {
-		var tr = $('<div/>');
+	var width = self.container.width() > WIDTH ? self.container.width() / 4 - 50 : self.container.width() - 50;
+	var specialCell = Math.floor((Math.random() * self.size * self.size));
+	// var specialCell = {
+	// 	col: Math.floor((Math.random() * self.size)),
+	// 	row: Math.floor((Math.random() * self.size))
+	// };
+	// for (var i = 0; i < self.size; i++) {
+		// var tr = $('<div/>');
 		// tr.addClass('container');
-		tr.css('clear', 'both');
-		tr.css('display', 'inline-block');
-		tr.css('height', width/self.size);
-		for (var j = 0; j < self.size; j++) {
+		// tr.css('clear', 'both');
+		// tr.css('display', 'inline-block');
+		// tr.css('height', width/self.size);
+		for (var j = 0; j < self.size*self.size; j++) {
 			var td = $('<div/>');
 
 			td.css('float', 'left');
-			td.css('width', width/self.size);
-			td.css('height', width/self.size);
+			td.css('width', 100/self.size + '%');
+			td.css('height', 100/self.size + '%');
 			td.css('border-radius', RADIUS);
 			td.css('background', color);
 			td.css('border', BORDER);
-			if (i == specialCell.row && j == specialCell.col) {
-				td.css('opacity', OPACITY);
-				td.click(function(e) {
-			        self.confirm(true);
-			    });
+			if (j == specialCell) {
+				td.css('opacity', self.opacity);
+				// td.click(function(e) {
+			 //        self.confirm(true);
+			 //    });
 				td[0].addEventListener("touchstart", function(e) {
 			        self.confirm(true);
 			    }, false);
 			} else {
-				td.click(function(e) {
-			        self.confirm(false);
-			    });
+				// td.click(function(e) {
+			 //        self.confirm(false);
+			 //    });
 				td[0].addEventListener("touchstart", function(e) {
 			        self.confirm(false);
 			    }, false);
 			}
-			tr.append(td);
+			box.append(td);
 		}
-		box.append(tr);
-	}
+	// 	box.append(tr);
+	// }
 	clearInterval(self.timer);
-	self.timing = TIME;
 	if (self.score != 0)
 		self.timer = setInterval(self.update, 1000);
 };
@@ -96,9 +109,10 @@ EyeTest.prototype.confirm = function(answer) {
 
 	if (answer) {
 		// next game
-		var score = $('.score', this.container);
+		var score = $('.score-count', this.container);
 		this.score += 1;
-		$('div', score).html(this.score);
+		self.timing = TIME;
+		score.html(this.score);
 		if (this.score  == 1) {
 			this.size += 1;
 		} else if (this.score == 4) {
@@ -110,26 +124,28 @@ EyeTest.prototype.confirm = function(answer) {
 				// this.level += 2;
 				this.count = 0;
 				this.opacity += 0.03;
+				console.log(self.opacity);
 			}
 		}
 
-		this.start(this.container);
+		this.start();
 	} else {
+		if (this.score != 0) {
 
-		var box = $('.eyetest-contain', this.container);
-		// degree time 2 seconds
-		// if time equal 0 then endgame
-		this.errors += 1;
-		$('.errors div', this.container).text(self.errors);
-		if (this.timing > 2) {
-			this.timing -= 2;
-			$('.count-down', self.container).text(self.timing);
+			var box = $('.eyetest-contain', this.container);
+			// degree time 2 seconds
+			// if time equal 0 then endgame
+			this.errors += 1;
+			$('.error-count', this.container).text(self.errors);
+			if (this.timing > 2) {
+				this.timing -= 2;
+				$('.time-count', self.container).text(self.timing);
+			}
+			else {
+				// end game
+				this.stop();
+			}
 		}
-		else {
-			// end game
-			this.stop();
-		}
-
 	}
 };
 EyeTest.prototype.update = function() {
@@ -138,10 +154,11 @@ EyeTest.prototype.update = function() {
 		self.stop();
 	self.timing -= 1;
 	// console.log(this.timing);
-	$('.count-down', self.container).text(self.timing);
+	$('.time-count', self.container).text(self.timing);
 };
 EyeTest.prototype.stop = function() {
 	var box = $('.eyetest-contain', this.container);
+	$('.time-count', this.container).text(0);
 	box.html(ui.gameover);
 	$('.start-game', box).click(function(e) {
 			        self.start();
